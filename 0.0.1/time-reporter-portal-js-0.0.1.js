983f02929d17f11beffdaabc6ea9cd229e667afe
commit <commit@noemail.com>
2012-09-05 21:16:01 +1200

    adding new module

---
+++ 0.0.1/time-reporter-portal-js-0.0.1.js
@@ -0,0 +1,16 @@
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/time-reporter-portal-js-0.0.1 -->
+
+(function($) {
+
+		$.initTimeReporterPortal = function(params) {
+
+			var elem = params.elem;
+
+			return {
+
+			};
+		};
+
+	})(jQuery);
+
+// <!-- one.end -->
