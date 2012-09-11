b1ca17e8adaf0b9eaa57f74786bb53b633065007
commit <commit@noemail.com>
2012-09-12 11:55:52 +1200

    completing implementation of data link

--- 0.0.1/time-reporter-portal-js-0.0.1.js
+++ 0.0.1/time-reporter-portal-js-0.0.1.js
@@ -2,48 +2,72 @@
 
 (function($) {
 
-		$.initTimeReporterPortal = function(params) {
+	$.initTimeReporterPortal = function(params) {
 
-			var elem = params.elem;
+		var elem = params.elem;
+		var client = params.client;
 
-			var form =  $.initTimeReporterForm({elem: $(".timereporterform", elem)});
-			
-			// init ui
-			(function() {
-				
-				
-				$('.reportTimeButton', elem).click(function(evt) {
-					evt.preventDefault();
-					
-					var operation= $('input[name=optionsRadios]:checked', elem).val();
-					
-					if (operation === "newPage") {
-						alert("Operation not supported yet.");
-						return;
-					}
-					
-					if (operation === "existingPage") {
-						
-						var nodeAddress = $('.nodeAddress', elem)
+		var form = $.initTimeReporterForm({
+			elem : $(".timereporterform", elem)
+		});
 
-						$('.portal', elem).hide();
-						
-						form.show(function(timeData) {
-							form.hide();
-							$('.portal', elem).show();
-							
-						});
-						
-						return;
-					}
-				});
-			}) ();
-			
-			return {
+		var data = $.initTimeReporterData({
+			client : client
+		});
 
-			};
+		// init ui
+		(function() {
+
+			$('.reportTimeButton', elem).click(
+					function(evt) {
+						evt.preventDefault();
+
+						var operation = $('input[name=optionsRadios]:checked',
+								elem).val();
+
+						if (operation === "newPage") {
+							alert("Operation not supported yet.");
+							return;
+						}
+
+						if (operation === "existingPage") {
+
+							var nodeAddress = $('.nodeAddress', elem)
+
+							$('.portal', elem).hide();
+							AJ.ui.showProgressBar();
+							client.load({
+								url : nodeAddress,
+								secret : AJ.userNodeSecret,
+								onSuccess : function(res) {
+									AJ.ui.hideProgressBar();
+									form.show(function(timeData) {
+										form.hide();
+										AJ.ui.showProgressBar();
+
+										data.submit(res.loadedNode, timeData);
+
+										client.commit({
+											onSuccess : function() {
+												AJ.ui.hideProgressBar();
+												$('.portal', elem).show();
+											}
+										});
+
+									});
+								}
+							});
+
+							return;
+						}
+					});
+		})();
+
+		return {
+
 		};
+	};
 
-	})(jQuery);
+})(jQuery);
 
 // <!-- one.end -->
